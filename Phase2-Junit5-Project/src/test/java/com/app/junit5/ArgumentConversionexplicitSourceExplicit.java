484cb8fc84f8cc4eed24d519a812138b30474cb3
patraasish <patraasish66@gmail.com>
2023-10-16 19:54:25 +0530

    Updated

---
+++ Phase2-Junit5-Project/src/test/java/com/app/junit5/ArgumentConversionexplicitSourceExplicit.java
@@ -0,0 +1,22 @@
+package com.app.junit5;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.EnumSource;
+
+public class ArgumentConversionexplicitSourceExplicit {
+	
+
+	@ParameterizedTest
+	@EnumSource(TimeUnit.class)
+	void testCaseWithExplicitArgumentConversion(
+	        @ConvertWith(ToStringArgumentConverter.class) String argument) {
+ 
+	    Assertions.assertNotNull(TimeUnit.valueOf(argument));
+	    System.out.println(argument);
+	}
+
+}
